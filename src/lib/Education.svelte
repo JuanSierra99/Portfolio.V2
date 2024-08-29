a24beac9886067a6b9fafb69da2b09763b9d21eb
Juan Sierra <sierra.juan99@gmail.com>
2024-08-28 22:10:06 -0700

    Create Education.svelte
    - A little education section would be nice. Something short and concise

---
+++ src/lib/Education.svelte
@@ -0,0 +1,4 @@
+<p>
+    I graduated from <span class="text-orange-500 font-bold">Cal State University of Fullerton</span> with a bachelor's degree in computer science with distinction, marking a significant moment in my career.
+  </p>
+  
