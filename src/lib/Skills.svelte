7f327f5be1aab3fb49f0286c41d102b8f275a83b
Juan Sierra <sierra.juan99@gmail.com>
2024-08-28 22:11:05 -0700

    Styling and content

--- src/lib/Skills.svelte
+++ src/lib/Skills.svelte
@@ -9,21 +9,26 @@ const skills: AutocompleteOption<string>[] = [
   {
     label: "Javascript",
     value: "javascript",
-    keywords: "languages, frontend, backend",
+    keywords: "frontend, backend",
   },
   {
     label: "Python",
     value: "python",
-    keywords: "languages, backend",
+    keywords: "backend",
   },
   {
     label: "Java",
     value: "java",
-    keywords: "languages, backend",
+    keywords: "backend",
   },
   {
     label: "C++",
     value: "c++",
+    keywords: "backend",
+  },
+  {
+    label: "NodeJs",
+    value: "nodejs",
     keywords: "backend",
   },
   {
@@ -42,6 +47,11 @@ const skills: AutocompleteOption<string>[] = [
     keywords: "frontend",
   },
   {
+    label: "SvelteKit",
+    value: "sveltekit",
+    keywords: "frontend, backend",
+  },
+  {
     label: "HTML",
     value: "html",
     keywords: "frontend",
@@ -57,28 +67,40 @@ const skills: AutocompleteOption<string>[] = [
     keywords: "frontend",
   },
   {
-    label: "Sql",
-    value: "sql",
-    keywords: "database, postgres",
+    label: "Postgres",
+    value: "postgres",
+    keywords: "backend",
+  },
+  {
+    label: "MySql",
+    value: "mysql",
+    keywords: "backend",
   },
   {
     label: "NGINX",
     value: "nginx",
-    keywords: "backend, server",
+    keywords: "backend",
+  },
+  {
+    label: "EC2",
+    value: "ec2",
+    keywords: "backend",
+  },
+  {
+    label: "Netlify",
+    value: "netlify",
+    keywords: "backend",
   },
 ];
 </script>
-
-<h2 class="font-bold text-3xl">Skills</h2>
   <div
-    class=" w-full max-w-full max-h-screen overflow-y-auto"
+    class=" w-full max-w-full max-h-screen overflow-y-auto card p-4"
     tabindex="-1"
   >
-    <RadioGroup display="flex" >
+    <RadioGroup display="flex" class={"mb-2"} >
     <RadioItem bind:group={skillFilter} name={"Filter"} value={""}>All</RadioItem>
     <RadioItem bind:group={skillFilter} name={"Filter"} value={"frontend"}>Frontend</RadioItem>
     <RadioItem bind:group={skillFilter} name={"Filter"} value={"backend"}>Backend</RadioItem>
-    <RadioItem bind:group={skillFilter} name={"Filter"} value={"languages"}>Languages</RadioItem>
   </RadioGroup>
-    <Autocomplete bind:input={skillFilter} options={skills} />
+    <Autocomplete bind:input={skillFilter} options={skills} regionItem="w-fit text-lg" regionList="flex flex-wrap gap-4" transitions={false}/>
   </div>
