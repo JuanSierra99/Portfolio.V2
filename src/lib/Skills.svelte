2b6bcf3715ec32fb095fd45351d4ab7fa9410602
Juan Sierra <sierra.juan99@gmail.com>
2024-08-28 22:44:34 -0700

    Replace radio group with button and search bar
    - 'Frontend' and 'Backend' buttons can be used to filter skills list for frontend and backend.
    - Added a 'X' button to clear the input

--- src/lib/Skills.svelte
+++ src/lib/Skills.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
       import { Autocomplete } from "@skeletonlabs/skeleton";
       import type { AutocompleteOption } from "@skeletonlabs/skeleton";
+      import IconClear from "~icons/mdi/clear-bold";
       import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
 
       let skillFilter: string = "";
@@ -97,10 +98,21 @@ const skills: AutocompleteOption<string>[] = [
     class=" w-full max-w-full max-h-screen overflow-y-auto card p-4"
     tabindex="-1"
   >
-    <RadioGroup display="flex" class={"mb-2"} >
+  <div class="mb-1">
+    <button class="btn variant-form-material text-xs" on:click={() => {skillFilter = "frontend"}}>Frontend</button>
+    <button class="btn variant-form-material text-xs" on:click={() => {skillFilter = "backend"}}>Backend</button>
+  </div>
+
+  <div class="relative flex items-center variant-ghost-surface rounded-md mb-2">
+    <input class="w-full p-2 bg-transparent hover:outline-none focus:outline-none" placeholder="Search..." bind:value={skillFilter}/>
+    <button class="hover:text-red-400 h-fit mr-2" on:click={() => {skillFilter = ""}}>
+      <IconClear/>
+    </button>
+  </div>
+    <!-- <RadioGroup display="flex" class={"mb-2"} >
     <RadioItem bind:group={skillFilter} name={"Filter"} value={""}>All</RadioItem>
     <RadioItem bind:group={skillFilter} name={"Filter"} value={"frontend"}>Frontend</RadioItem>
     <RadioItem bind:group={skillFilter} name={"Filter"} value={"backend"}>Backend</RadioItem>
-  </RadioGroup>
+  </RadioGroup> -->
     <Autocomplete bind:input={skillFilter} options={skills} regionItem="w-fit text-lg" regionList="flex flex-wrap gap-4" transitions={false}/>
   </div>
