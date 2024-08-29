260253b3ea5bdca3c5e76922260b101f1c58e264
Juan Sierra <sierra.juan99@gmail.com>
2024-08-28 22:11:19 -0700

    Add education section and underline Headers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import { Avatar } from "@skeletonlabs/skeleton";
   import Projects from "$lib/Projects.svelte";
   import Skills from "$lib/Skills.svelte";
+  import Education from "$lib/Education.svelte";
 </script>
 
 <div class="container mx-auto p-8 space-y-8 max-w-3xl">
@@ -13,7 +14,10 @@
     <strong class="text-gray-400">1</strong> A fullstack developer navigating the
     complex yet beautiful world of web development.
   </p>
+  <h2 class="font-bold text-3xl underline">Skills</h2>
   <Skills/>
-  <h2 class="font-bold text-3xl">Projects</h2>
+  <h2 class="font-bold text-3xl underline">Projects</h2>
   <Projects/>
+  <h2 class="font-bold text-3xl underline">Education</h2>
+  <Education/>
 </div>
