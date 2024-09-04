f0a43e47418cdd94e7ed3f3f5775f8a9a67ae934
Juan Sierra <sierra.juan99@gmail.com>
2024-09-04 00:08:26 -0700

    Update Education.svelte

--- src/lib/Education.svelte
+++ src/lib/Education.svelte
@@ -1,4 +1,4 @@
 <p>
-    I graduated from <span class="text-orange-500 font-bold">Cal State University of Fullerton</span> with a bachelor's degree in computer science with distinction, marking a significant moment in my career.
+    I graduated with distinction from <span class="text-orange-500 font-bold">Cal State University of Fullerton</span> with a bachelor's degree in computer science. This achievement marked a significant moment in my career.
   </p>
   
